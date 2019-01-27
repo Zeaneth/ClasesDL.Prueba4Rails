ae44c065d3fe8ec565af6ef05bd548cbebb20e82
Zamiz <zamiztobar@gmail.com>
2019-01-27 20:31:53 -0300

    controller: add 'done: false' as param included in new Todo creation style: translate devise authentication message when asked to sign in

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,13 +1,39 @@
 class TodosController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_todo, only: [:show, :edit, :update, :destroy]
+
   def index
+    @todos = current_user.todos
+  end
+
+  def show
   end
 
   def create
+    @task = Task.find(params[:task_id])
+    @todo = Todo.new(task: @task, user: current_user, done: false)
+    if @todo.save!
+      redirect_to tasks_path, notice: 'Se ha creado un nuevo todo :)'
+    else
+      redirect_to tasks_path, alert: 'No se ha podido crear el todo :('
+    end
   end
 
   def delete
+    
   end
 
   def edit
   end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:task_id, :done, :done_at)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
+
 end
