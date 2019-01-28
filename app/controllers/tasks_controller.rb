362667ebdff473d534a4718c066d1a38fd7949a4
Zamiz <zamiztobar@gmail.com>
2019-01-28 01:28:29 -0300

    style: add ranking card view to tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:ranking, :edit, :update, :destroy]
 
   # GET /tasks
   # GET /tasks.json
@@ -9,7 +9,7 @@ class TasksController < ApplicationController
 
   # GET /tasks/1
   # GET /tasks/1.json
-  def show
+  def ranking
   end
 
   # GET /tasks/new
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  root to: 'tasks#index'
 
   resources :tasks do
     resources :todos, only: :create
   end
+  get 'tasks/:id/ranking', to: 'tasks#ranking', as: 'tasks_ranking'
 
   resources :todos, only: :index
   
   devise_for :users
 
-  root to: 'tasks#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
